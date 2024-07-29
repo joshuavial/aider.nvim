c963699b8197b2b3c79382cd1d03ee60fd318011
Jon Kinney <jonkinney@gmail.com>
2024-07-29 09:57:11 -0500

    Added new keybindings and updated existing ones for the Aider plugin.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,16 +2,16 @@ vim.g.mapleader = vim.g.mapleader or " "
 
 local wk = require("which-key")
 
-wk.register({
-  { "<leader>A", group = "Aider" },
-  { "<leader>AB", ":AiderBackground -3<CR>", desc = "Run Aider (GPT-3.5) in Background" },
-  { "<leader>AO", ":AiderOpen -3<CR>", desc = "Open Aider (GPT-3.5)" },
-  { "<leader>Ab", ":AiderBackground<CR>", desc = "Run Aider in Background" },
-  { "<leader>Am", ":AiderAddModifiedFiles<CR>", desc = "Add Modified Files to Chat" },
-  { "<leader>Ao", ":AiderOpen<CR>", desc = "Open Aider" },
+wk.add({
+  { "<leader>A",  group = "Aider" }, -- group
+  { "<leader>AB", "<CMD>AiderBackground -3<CR>",    desc = "Run Aider (GPT-3.5) in Background", mode = "n" },
+  { "<leader>AO", "<CMD>AiderOpen -3<CR>",          desc = "Open Aider (GPT-3.5)",              mode = "n" },
+  { "<leader>Ab", "<CMD>AiderBackground<CR>",       desc = "Run Aider in Background",           mode = "n" },
+  { "<leader>Am", "<CMD>AiderAddModifiedFiles<CR>", desc = "Add Modified Files to Chat",        mode = "n" },
+  { "<leader>Ao", "<CMD>AiderOpen<CR>",             desc = "Open Aider",                        mode = "n" },
 })
 
--- Set up the actual keybindings
+-- Set up the actual keybindings (these are redundant if using which-key, but included for completeness)
 vim.keymap.set("n", "<leader>AB", "<CMD>AiderBackground -3<CR>", { desc = "Run Aider (GPT-3.5) in Background" })
 vim.keymap.set("n", "<leader>AO", "<CMD>AiderOpen -3<CR>", { desc = "Open Aider (GPT-3.5)" })
 vim.keymap.set("n", "<leader>Ab", "<CMD>AiderBackground<CR>", { desc = "Run Aider in Background" })
